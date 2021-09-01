a2acd7208e346e7be88381a019205ba2890b9197
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-09-01 20:30:28 +0600

    Added post request

--- pymyst/client.py
+++ pymyst/client.py
@@ -10,10 +10,13 @@ def get(endpoint: str, auth: str = None) -> str:
     return requests.get(endpoint, headers=headers).text
 
 
-def post():
-    pass
-    # TODO: Add post requests
+def post(endpoint: str, auth: str, content_type: str='application/json') -> str:
+    headers = {'Content-Type': content_type}
 
+    if auth is not None:
+        headers['Authorization'] = auth
+
+    return requests.get(endpoint, headers=headers).text
 
 def delete():
     pass
