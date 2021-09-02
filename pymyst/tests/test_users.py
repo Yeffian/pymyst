64469992bd2ac7262b75b415f2944873f9f4000a
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-09-02 15:21:52 +0600

    Changed the tests to assert values from a dictionary and not a class

--- pymyst/tests/test_users.py
+++ pymyst/tests/test_users.py
@@ -5,10 +5,10 @@ def test_get_user():
     response = user.data()
 
     assert isinstance(response, dict)
-    assert user.name == 'CodeMyst'
-    assert user.id == 'bcfu7961'
-    assert user.lang == 'D'
-    assert user.is_contributer == True
-    assert user.avatar_url ==  'https://avatars3.githubusercontent.com/u/7966628?v=4'
-    assert user.is_public_profile == True
+    assert user['name'] == 'CodeMyst'
+    assert user['id'] == 'bcfu7961'
+    assert user['lang'] == 'D'
+    assert user['is_contributer'] == True
+    assert user['avatar_url'] ==  'https://avatars3.githubusercontent.com/u/7966628?v=4'
+    assert user['is_public_profile'] == True
 
