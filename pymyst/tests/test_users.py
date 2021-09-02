1eb0e32cb133cd88bae7e4e3a6245ee2fc170c14
Justin <contact@tsujin.com>
2021-09-02 15:26:54 -0400

    rearranged data fields to match json order due to OCD, fixed the uncallable dictionary

--- pymyst/tests/test_users.py
+++ pymyst/tests/test_users.py
@@ -2,13 +2,13 @@ from pymyst.types.User import User
 
 def test_get_response():
     user = User('codemyst')
-    response = user.data()
+    response = user.data
 
     assert isinstance(response, dict)
     assert response['username'] == 'CodeMyst'
     assert response['contributor'] == True
     assert response['_id'] == 'bcfu7961'
-    assert response['defaultLang'] == 'D'
     assert response['avatarUrl'] ==  'https://avatars3.githubusercontent.com/u/7966628?v=4'
     assert response['publicProfile'] == True
+    assert response['defaultLang'] == 'D'
     assert response['supporterLength'] == 0
