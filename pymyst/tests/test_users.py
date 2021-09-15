581244f4807979da5adac26093b2ec881f65ff2f
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-09-15 21:42:15 +0600

    Added get_user_from_user_token, added required unit tests and added data file

--- pymyst/tests/test_users.py
+++ pymyst/tests/test_users.py
@@ -1,4 +1,28 @@
+import json
 from pymyst.types.User import User
+
+
+def load_config():
+    return json.load(open('data.json'))
+
+
+def test_get_from_user_token():
+    user = User.get_from_user_token(load_config()['token'])
+
+    response = user.data
+
+    assert isinstance(response, dict)
+
+    assert isinstance(response['username'], str)
+    assert isinstance(response['contributor'], bool)
+    assert isinstance(response['_id'], str)
+    assert isinstance(response['avatarUrl'], str)
+    assert isinstance(response['publicProfile'], bool)
+    assert isinstance(response['defaultLang'], str)
+    assert isinstance(response['supporterLength'], int)
+
+    assert isinstance(response['stars'], list)
+    assert isinstance(response['serviceIds'], dict)
 
 
 def test_get_from_name():
