ca362b7a75479aa1b89eb538b31c8b57ea70b329
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-01-26 16:25:04 +0600

    added unit tests for create_paste

--- pymyst/tests/test_paste.py
+++ pymyst/tests/test_paste.py
@@ -62,4 +62,18 @@ def test_create_paste():
         token.API_TOKEN
     )
 
-    assert paste.data is not None
+    data = paste.data
+
+    assert isinstance(data, dict)
+    assert isinstance(data['_id'], str)
+    assert isinstance(data['ownerId'], str)
+    assert isinstance(data['title'], str)
+    assert isinstance(data['createdAt'], int)
+    assert isinstance(data['expiresIn'], str)
+    assert isinstance(data['deletesAt'], int)
+    assert isinstance(data['stars'], int)
+    assert isinstance(data['isPrivate'], bool)
+    assert isinstance(data['isPublic'], bool)
+    assert isinstance(data['tags'], list)
+    assert isinstance(data['pasties'], list)
+    assert isinstance(data['edits'], list)
